2d9599ff8405d44c3ed30a9b2a7935f4b74820f7
aroraarjita <a.arjita1601@gmail.com>
2024-07-05 23:21:10 +0530

    Console Arguments

---
+++ ConsoleArguments/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+
+        public static readonly string test = new string('0', 40);
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(test);
+            Console.ReadLine();
+
+        }
+    }
+}
