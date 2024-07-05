553584909c773a2f10139b3a6bff1b19fe8d55a3
aroraarjita <a.arjita1601@gmail.com>
2024-07-05 23:16:59 +0530

    Max Frequency

---
+++ MaxFrequency/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        public int solution(int[] A, int[] B, int N)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+
+
+            // group by value and count frequency
+            var query = from i in A.ToList()
+                        group i by i into g
+                        select new { g.Key, Count = g.Count() };
+
+            // compute the maximum frequency
+            int whatsTheFrequencyKenneth = query.Max(g => g.Count);
+
+
+
+        }
+    }
+}
