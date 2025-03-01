cfe1b26438440ceb60023644a39de334544d9b97
aroraarjita <a.arjita1601@gmail.com>
2025-03-01 14:29:39 +0530

    Dunhumbby Chatgpt questions

---
+++ DunHummby_Questions/DuplicatesInAnArray/DuplicatesInAnArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DuplicatesInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 2, 5, 1 };
+            List<int> duplicates = FindDuplicates(nums);
+            Console.WriteLine(string.Join("," , duplicates));
+
+            Console.ReadLine();
+        }
+
+        static List<int> FindDuplicates(int[] nums)
+        {
+
+            return nums.GroupBy(x => x)
+                       .Where(g => g.Count() > 1)
+                        .Select(g => g.Key)
+                        .ToList();
+        }
+    }
+}
---
+++ DunHummby_Questions/ReverseWordsInAString/ReverseWordsInAString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseWordsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Lead Software Developer";
+            string output = ReverseWords(input);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+        static string ReverseWords(string str)
+        {
+            System.Collections.Generic.IEnumerable<string> arrayOfWords = str.Split(' ').Reverse();
+            return string.Join(" ", arrayOfWords);
+
+        }
+    }
+}
